88e0985082f18641a7acd75c9555beec586a49a7
CommandPrompt553 <commandpromptemailcontact@gmail.com>
2026-07-10 10:41:35 +0200

    Update resetgameover2.js

--- resetgameover2.js
+++ resetgameover2.js
@@ -25,41 +25,4 @@ Runner.instance_.gameOver = function() {
 
             // Reset the time clock.
             this.time = getTimeStamp();
-        },
-
-        stop: function () {
-            this.playing = false;
-            this.paused = true;
-            cancelAnimationFrame(this.raqId);
-            this.raqId = 0;
-        },
-
-        play: function () {
-            if (!this.crashed) {
-                this.playing = true;
-                this.paused = false;
-                this.tRex.update(0, Trex.status.RUNNING);
-                this.time = getTimeStamp();
-                this.update();
-            }
-        },
-
-        restart: function () {
-            if (!this.raqId) {
-                this.playCount++;
-                this.runningTime = 0;
-                this.playing = true;
-                this.crashed = false;
-                this.distanceRan = 0;
-                this.setSpeed(this.config.SPEED);
-                this.time = getTimeStamp();
-                this.containerEl.classList.remove(Runner.classes.CRASHED);
-                this.clearCanvas();
-                this.distanceMeter.reset(this.highestScore);
-                this.horizon.reset();
-                this.tRex.reset();
-                this.playSound(this.soundFx.BUTTON_PRESS);
-                this.invert(true);
-                this.update();
-            }
         };
